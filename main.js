028b2a5f1a6b1a4d7a01466bdb4033698efc207b
priyansh11k <80451561+priyansh11k@users.noreply.github.com>
2022-02-09 11:56:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 harry = "";
 beliver = "";
+leftWristX = "";
+leftWristY = "";
+ rightWristX= "";
+rightWristY = "";
 
 
 
 function preload(){
     harry = loadSound("music.mp3");
-    beliver = loadSound("Beliver.mp3");
+    beliver = loadSound("Believer.mp3");
 }
 
 
@@ -15,8 +19,29 @@ canvas.center();
 
 video = createCapture(VIDEO);
 video.hide();
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose' , gotPoses);
 }
 
 function draw(){
 image(video,0,0,600,500);
 }
+
+function modelLoaded(){
+    console.log("Posenet is Ready");
+    }
+
+    function gotPoses(results){
+    if(results.length > 0){
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("left wrist x = " + leftWristX + "left wrist y = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("right wrist x = " + rightWristX + "right wrist y = " + rightWristY);
+
+    }
+    }
